b09a47f5159a413b34d5d11b9debfc3d34737893
Sassan Zomorodi <sassanzomorodi@yahoo.com>
2023-11-17 14:53:19 +0100

    Refactored TextFormatter
    Broke down rewriteListInHTML() into smaller functions

--- src/text-formatter.ts
+++ src/text-formatter.ts
@@ -22,86 +22,98 @@ export class TextFormatter {
     private rewriteListInHtml() {
         let output = this._output.split('\n');
         let newOutput: string[] = [];
-
+    
         if (this.isOrderedList(this._output)) {
-            let listOngoing = false;
-            
-            output.forEach((line) => {
-                let newline: string = line.trim();
-
-                if (this.isOrderedList(newline)) {
-                    if (this.isTitledList(newline)) {
-                        let colonIndex = newline.indexOf(':');
-                        newline = "<li>" + "<b>" + newline.slice(3, colonIndex+1) + "</b>" + newline.slice(colonIndex+1,) + "</li>";
-                    }
-                    else {
-                        newline = "<li>" + newline.slice(3,) + "</li>";
-                    }
-                    if (!listOngoing) {
-                        listOngoing = true;
-                        newline = "<ol>" + newline;
-                    }
-                }
-                else if (newline == '' || newline == '\n') {
-                    return;
-                }
-                else if (listOngoing) {
-                    listOngoing = false;
-                    newline = "</ol>" + newline;
-                }
-
-                newOutput.push(newline);
-            })
-
-            newOutput.push("</ol>");
+            newOutput = this.rewriteOrderedList(output);
+        } else if (this.isUnorderedList(this._output)) {
+            newOutput = this.rewriteUnorderedList(output);
+        } else {
+            newOutput = this.rewriteNormalList(output);
         }
-        else if (this.isUnorderedList(this._output)) {
-            let listOngoing = false;
-            
-            output.forEach((line) => {
-                let newline: string = line.trim();
-
-                if (this.isUnorderedList(newline)) {
-                    if (this.isTitledList(newline)) {
-                        let colonIndex = newline.indexOf(':');
-                        newline = "<li>" + "<b>" + newline.slice(2, colonIndex+1) + "</b>" + newline.slice(colonIndex+1,) + "</li>";
-                    }
-                    else {
-                        newline = "<li>" + newline.slice(2,) + "</li>";
-                    }
-                    if (!listOngoing) {
-                        listOngoing = true;
-                        newline = "<ul>" + newline;
-                    }
-                }
-                else if (newline == '' || newline == '\n') {
-                    return;
-                }
-                else if (listOngoing) {
-                    listOngoing = false;
-                    newline = "</ul>" + newline;
-                }
-
-                newOutput.push(newline);
-            })
-
-            newOutput.push("</ul>");
-        }
-        else {
-            output.forEach((line) => {
-                let newline: string = line.trim();
-
-                if (this.isTitledList(newline)) {
-                    let colonIndex = newline.indexOf(':');
-                    newline = "<b>" + newline.slice(0, colonIndex+1) + "</b>" + newline.slice(colonIndex+1);
-                }
-
-                newOutput.push(newline);
-            })
-        }
-
+    
         this._output = newOutput.join('\n\n');
     }
+    
+    private rewriteOrderedList(output: string[]): string[] {
+        let newOutput: string[] = [];
+        let listOngoing = false;
+    
+        output.forEach((line) => {
+            let newline: string = line.trim();
+    
+            if (this.isOrderedList(newline)) {
+                if (this.isTitledList(newline)) {
+                    let colonIndex = newline.indexOf(':');
+                    newline = "<li>" + "<b>" + newline.slice(3, colonIndex + 1) + "</b>" + newline.slice(colonIndex + 1,) + "</li>";
+                } else {
+                    newline = "<li>" + newline.slice(3,) + "</li>";
+                }
+                if (!listOngoing) {
+                    listOngoing = true;
+                    newline = "<ol>" + newline;
+                }
+            } else if (newline == '' || newline == '\n') {
+                return;
+            } else if (listOngoing) {
+                listOngoing = false;
+                newline = "</ol>" + newline;
+            }
+    
+            newOutput.push(newline);
+        });
+    
+        newOutput.push("</ol>");
+        return newOutput;
+    }
+    
+    private rewriteUnorderedList(output: string[]): string[] {
+        let newOutput: string[] = [];
+        let listOngoing = false;
+    
+        output.forEach((line) => {
+            let newline: string = line.trim();
+    
+            if (this.isUnorderedList(newline)) {
+                if (this.isTitledList(newline)) {
+                    let colonIndex = newline.indexOf(':');
+                    newline = "<li>" + "<b>" + newline.slice(2, colonIndex + 1) + "</b>" + newline.slice(colonIndex + 1,) + "</li>";
+                } else {
+                    newline = "<li>" + newline.slice(2,) + "</li>";
+                }
+                if (!listOngoing) {
+                    listOngoing = true;
+                    newline = "<ul>" + newline;
+                }
+            } else if (newline == '' || newline == '\n') {
+                return;
+            } else if (listOngoing) {
+                listOngoing = false;
+                newline = "</ul>" + newline;
+            }
+    
+            newOutput.push(newline);
+        });
+    
+        newOutput.push("</ul>");
+        return newOutput;
+    }
+    
+    private rewriteNormalList(output: string[]): string[] {
+        let newOutput: string[] = [];
+    
+        output.forEach((line) => {
+            let newline: string = line.trim();
+    
+            if (this.isTitledList(newline)) {
+                let colonIndex = newline.indexOf(':');
+                newline = "<b>" + newline.slice(0, colonIndex + 1) + "</b>" + newline.slice(colonIndex + 1);
+            }
+    
+            newOutput.push(newline);
+        });
+    
+        return newOutput;
+    }    
 
     private isOrderedList(text: string):boolean {
         const ordredListPattern = /\d+\.\s+/;
